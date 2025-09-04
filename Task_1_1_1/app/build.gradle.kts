2818e466de0f817937ebd4ed9819ca37a35db8b6
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-04 10:32:52 +0700

    fix

--- Task_1_1_1/app/build.gradle.kts
+++ Task_1_1_1/app/build.gradle.kts
@@ -48,3 +48,10 @@ tasks.jacocoTestReport {
     }
 }
 
+tasks {
+    withType<JavaCompile>().configureEach { options.encoding = "UTF-8" }
+    withType<JavaExec>().configureEach { defaultCharacterEncoding = "UTF-8" }
+    withType<Javadoc>().configureEach { options.encoding = "UTF-8" }
+    withType<Test>().configureEach { defaultCharacterEncoding = "UTF-8" }
+}
+
