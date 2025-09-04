77ab11818e42ee29b1ff26a9a428af908834255a
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-04 16:30:31 +0700

    fix

--- Task_1_1_1/app/src/test/java/org/example/AppTest.java
+++ Task_1_1_1/app/src/test/java/org/example/AppTest.java
@@ -2,28 +2,28 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.junit.jupiter.api.Test;
-
 public class AppTest {
-  @Test
-  public void sortTest() {
-    int arr[] = { 9, 8, 7, 6, 5, 4, 3 };
-    int expected[] = { 3, 4, 5, 6, 7, 8, 9 };
-    App.sort(arr);
-    assertArrayEquals(arr, expected);
-  }
+    @Test
+    public void sortTest() {
+        int arr[] = {9, 8, 7, 6, 5, 4, 3};
+        int expected[] = {3, 4, 5, 6, 7, 8, 9};
+        App.sort(arr);
+        assertArrayEquals(arr, expected);
+    }
 
-  @Test
-  public void mainTest() {
-    final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+    @Test
+    public void mainTest() {
+        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
 
-    System.setOut(new PrintStream(myOut));
-    App.main(new String[] {});
-    
-    final String standardOutput = myOut.toString();
-    assertEquals(standardOutput, "[1, 2, 3, 4, 5]\n");
-  }
+        System.setOut(new PrintStream(myOut));
+        App.main(new String[] {});
+
+        final String standardOutput = myOut.toString();
+        assertEquals(standardOutput, "[1, 2, 3, 4, 5]\n");
+    }
 }
