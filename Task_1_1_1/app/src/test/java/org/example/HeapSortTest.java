7a24cd86c4602af71fdeee17152809b88dbea1af
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-05 23:26:43 +0700

    add some tests

--- Task_1_1_1/app/src/test/java/org/example/HeapSortTest.java
+++ Task_1_1_1/app/src/test/java/org/example/HeapSortTest.java
@@ -1,16 +1,52 @@
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.Arrays;
+import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 
 public class HeapSortTest {
     @Test
-    public void testSimple() {
-        int[] arr = {5, 4, 3, 2, 1};
+    public void testPositive() {
+        int[] arr = {5, 4, 7, 2, 1};
         HeapSort.sort(arr);
 
-        int[] expected = {1, 2, 3, 4, 5};
+        int[] expected = {1, 2, 4, 5, 7};
         assertArrayEquals(expected, arr);
+    }
+    
+    @Test
+    public void testNegative() {
+        int[] arr = {-2, -1, -4, -8, -5};
+        HeapSort.sort(arr);
+
+        int[] expected = {-8, -5, -4, -2, -1};
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void testEmpty() {
+        int[] arr = new int[0];
+        assertDoesNotThrow(() -> HeapSort.sort(arr));
+        assertArrayEquals(new int[0], arr);
+    }
+    
+    @Test
+    public void testRandom() {
+        Random random = new Random();
+        
+        int[] arr = new int[10000];        
+        for (int i = 0; i < 10000; i++)
+            arr[i] = random.nextInt();
+
+        int[] arr_copy = arr.clone();
+
+        HeapSort.sort(arr);
+        Arrays.sort(arr_copy);
+
+        assertArrayEquals(arr_copy, arr);
     }
 }
