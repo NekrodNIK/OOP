4436a50ef1dd5c5420762b96a93ac34422ab0d0b
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-05 22:23:43 +0700

    add javadoc

--- Task_1_1_1/app/src/main/java/org/example/HeapSort.java
+++ Task_1_1_1/app/src/main/java/org/example/HeapSort.java
@@ -1,6 +1,12 @@
 package org.example;
 
+/** The heap sorting algorithm. */
 public class HeapSort {
+    /**
+     * The entry point.
+     *
+     * @param arr input array
+     */
     public static void sort(int[] arr) {
         for (int i = arr.length / 2 - 1; i >= 0; i--) {
             siftDown(arr, arr.length, i);
@@ -12,6 +18,13 @@ public class HeapSort {
         }
     }
 
+    /**
+     * Sifting down an item in the heap.
+     *
+     * @param arr input array
+     * @param n number of item
+     * @param i start index
+     */
     private static void siftDown(int[] arr, int n, int i) {
         for (; ; ) {
             int j = i;
@@ -28,16 +41,35 @@ public class HeapSort {
         }
     }
 
+    /**
+     * Swap the two items.
+     *
+     * @param arr input array
+     * @param i first item
+     * @param j second item
+     */
     private static void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
 
+    /**
+     * Get the left child index.
+     *
+     * @param i parent index
+     * @return left child index
+     */
     private static int left(int i) {
         return (i + 1) * 2 - 1;
     }
 
+    /**
+     * Get the right child index.
+     *
+     * @param i parent index
+     * @return right child index
+     */
     private static int right(int i) {
         return (i + 1) * 2;
     }
