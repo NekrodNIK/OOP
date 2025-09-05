da76693571059c606a910ac04063d56f8e635bfb
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-05 21:59:13 +0700

    fix

--- Task_1_1_1/manually.sh
+++ Task_1_1_1/manually.sh
@@ -1,9 +1,14 @@
 JAVA_SRCS=$(find app/src/main -name '*.java')
 JAR='app.jar'
 MANIFEST='MANIFEST.mf'
+
 BUILD_DIR='builddir'
+DOCS_DIR='build/docs'
 
 javac -d $BUILD_DIR $JAVA_SRCS
+
+javadoc -d $DOCS_DIR $JAVA_SRCS
+
 jar -cfm $JAR $MANIFEST -C $BUILD_DIR .
 java -jar $JAR
 
