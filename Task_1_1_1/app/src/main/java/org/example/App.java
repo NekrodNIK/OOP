3ba13962c0a3db2d09726f7dbe1d1a9ce5238c47
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-05 22:06:12 +0700

    fix

--- Task_1_1_1/app/src/main/java/org/example/App.java
+++ Task_1_1_1/app/src/main/java/org/example/App.java
@@ -2,10 +2,12 @@ package org.example;
 
 import java.util.Arrays;
 
+/** A class containing the entry point. */
 public class App {
-    public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
-        HeapSort.sort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
+  /** The entry point. */
+  public static void main(String[] args) {
+    int[] arr = { 5, 4, 3, 2, 1 };
+    HeapSort.sort(arr);
+    System.out.println(Arrays.toString(arr));
+  }
 }
