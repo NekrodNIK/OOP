fa5b97cc3b15c12f96ac219d40030136b5d84b77
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-06 00:10:00 +0700

    checkstyle

--- Task_1_1_1/app/src/test/java/org/example/HeapSortTest.java
+++ Task_1_1_1/app/src/test/java/org/example/HeapSortTest.java
@@ -8,8 +8,10 @@ import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 
+
 // ** Test for heap sorting algorithm */
 public class HeapSortTest {
+    // ** Test for positive items. */
     @Test
     public void testPositive() {
         int[] arr = {5, 4, 7, 2, 1};
@@ -19,6 +21,7 @@ public class HeapSortTest {
         assertArrayEquals(expected, arr);
     }
 
+    // ** Test for negative items. */
     @Test
     public void testNegative() {
         int[] arr = {-2, -1, -4, -8, -5};
@@ -28,6 +31,7 @@ public class HeapSortTest {
         assertArrayEquals(expected, arr);
     }
 
+    // ** Test for empty items. */
     @Test
     public void testEmpty() {
         int[] arr = new int[0];
@@ -35,6 +39,7 @@ public class HeapSortTest {
         assertArrayEquals(new int[0], arr);
     }
 
+    // ** Test for single item. */
     @Test
     public void testSingle() {
         int[] arr = {2};
@@ -42,6 +47,7 @@ public class HeapSortTest {
         assertArrayEquals(new int[] {2}, arr);
     }
 
+    // ** Test for alredy sorted arr. */
     @Test
     public void testSorted() {
         int[] sorted = {1, 2, 3, 4, 5};
@@ -49,6 +55,7 @@ public class HeapSortTest {
         assertArrayEquals(new int[] {1, 2, 3, 4, 5}, sorted);
     }
 
+    // ** Test for reverse sorted arr. */
     @Test
     public void testReverseOrderSorted() {
         int[] reversed = {5, 4, 3, 2, 1};
@@ -56,6 +63,7 @@ public class HeapSortTest {
         assertArrayEquals(new int[] {1, 2, 3, 4, 5}, reversed);
     }
 
+    // ** Test for dublicated items. */
     @Test
     public void testWithEqualItems() {
         int[] arr = {2, 3, 1, 3, 4, 2};
@@ -63,18 +71,21 @@ public class HeapSortTest {
         assertArrayEquals(new int[] {1, 2, 2, 3, 3, 4}, arr);
     }
 
+    // ** Randomized items test. */
     @Test
     public void testRandom() {
         Random random = new Random();
 
         int[] arr = new int[100000];
-        for (int i = 0; i < 100000; i++) arr[i] = random.nextInt();
+        for (int i = 0; i < 100000; i++) {
+            arr[i] = random.nextInt();
+        }
 
-        int[] arr_copy = arr.clone();
+        int[] copy = arr.clone();
 
         HeapSort.sort(arr);
-        Arrays.sort(arr_copy);
+        Arrays.sort(copy);
 
-        assertArrayEquals(arr_copy, arr);
+        assertArrayEquals(copy, arr);
     }
 }
