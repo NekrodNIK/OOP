7d2e7c632d451aa9764a4faa9c1622d2a97b01f2
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-30 13:21:16 +0700

    fix

--- Task_1_1_3/lib/src/main/java/sys/pro/Mul.java
+++ Task_1_1_3/lib/src/main/java/sys/pro/Mul.java
@@ -5,7 +5,7 @@ public class Mul extends BinaryOperator {
     super(lhs, rhs);
     symbol = '*';
   }
-  
+
   @Override
   public Expression derivative(Variable var) {
     return new Add(new Mul(lhs.derivative(var), rhs), new Mul(lhs, rhs.derivative(var)));
@@ -13,6 +13,33 @@ public class Mul extends BinaryOperator {
 
   @Override
   protected Integer calcOperator(Integer a, Integer b) {
-    return a*b;
+    return a * b;
+  }
+
+  @Override
+  public Expression simplify() {
+    Expression some = super.simplify();
+    if (some instanceof Number) {
+      return some;
+    }
+
+    Expression sLhs = lhs.simplify();
+    Expression sRhs = rhs.simplify();
+
+    if (sLhs instanceof Number || sRhs instanceof Number) {
+      Number sLhsNum = (Number) sLhs;
+      Number sRhsNum = (Number) sRhs;
+      if (sLhsNum.getValue() == 0 || sRhsNum.getValue() == 0) {
+        return new Number(0);
+      }
+      if (sLhsNum.getValue() == 1) {
+        return sRhsNum;
+      }
+      if (sRhsNum.getValue() == 1) {
+        return sLhsNum;
+      }
+    }
+
+    return this;
   }
 }
--- Task_1_1_3/lib/src/main/java/sys/pro/Sub.java
+++ Task_1_1_3/lib/src/main/java/sys/pro/Sub.java
@@ -15,4 +15,21 @@ public class Sub extends BinaryOperator {
   protected Integer calcOperator(Integer a, Integer b) {
     return a-b;
   }  
+  
+  @Override
+  public Expression simplify() {
+    Expression some = super.simplify();
+    if (some instanceof Number) {
+      return some;
+    }
+
+    Expression sLhs = lhs.simplify();
+    Expression sRhs = rhs.simplify();
+
+    if (sLhs.equals(sRhs)) {
+      return new Number(0);
+    }
+
+    return this;
+  }
 }
