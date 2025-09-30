c1d1867776cdeab01268c433e5334bf976ad058e
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-30 13:38:37 +0700

    fix

--- Task_1_1_3/lib/src/test/java/sys/pro/SimplifyTest.java
+++ Task_1_1_3/lib/src/test/java/sys/pro/SimplifyTest.java
@@ -11,4 +11,22 @@ public class SimplifyTest {
         Expression expected = new Sub(new Mul(new Number(111), new Variable("x")), new Number(77));
         assertEquals(expected, Parser.parse(input).simplify());
     }
+    
+    @Test
+    void testMulZero() {
+        String input = "x*0";
+        Expression expected = new Number(0);
+        assertEquals(expected, Parser.parse(input).simplify());
+        input = "0*x";
+        assertEquals(expected, Parser.parse(input).simplify());
+    }
+    
+    @Test
+    void testMulOne() {
+        String input = "x*1";
+        Expression expected = new Variable("x");
+        assertEquals(expected, Parser.parse(input).simplify());
+        input = "1*x";
+        assertEquals(expected, Parser.parse(input).simplify());
+    }
 }
