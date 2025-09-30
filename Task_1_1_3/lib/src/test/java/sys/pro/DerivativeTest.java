92a0318f86cba930020e69a1c8e84c171b61e579
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-30 13:12:01 +0700

    fix

--- Task_1_1_3/lib/src/test/java/sys/pro/DerivativeTest.java
+++ Task_1_1_3/lib/src/test/java/sys/pro/DerivativeTest.java
@@ -1,11 +1,13 @@
 package sys.pro;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class DerivativeTest {
   @Test
   void testExpr() {
-    System.out.println(Parser.parse("x+x+y+z+1+2*x+2/x-x").derivative(new Variable("x")));
+    assertEquals(Parser.parse("1+1+0+0+0+2-1+0"), Parser.parse("x+x+y+z+1+2*x-x+x/x").derivative(new Variable("x")));
   }
 }
