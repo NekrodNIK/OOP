b151dc7219e02b3a2d2709f6ddb103189d3a0c1b
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-10-05 23:54:03 +0700

    fix

--- Task_1_1_3/lib/src/test/java/sys/pro/DerivativeTest.java
+++ Task_1_1_3/lib/src/test/java/sys/pro/DerivativeTest.java
@@ -11,4 +11,67 @@ public class DerivativeTest {
     assertEquals(new Add(new Number(1), new Number(0)),
         new Add(new Variable("x"), new Variable("y")).derivative(new Variable("x")));
   }
+
+  @Test
+  void testSub() {
+    assertEquals(new Sub(new Number(1), new Number(0)),
+        new Sub(new Variable("x"), new Variable("y")).derivative(new Variable("x")));
+  }
+
+  @Test
+  void testMul() {
+    assertEquals(new Add(new Mul(new Number(1), new Variable("y")), new Mul(new Variable("x"), new Number(0))),
+        new Mul(new Variable("x"), new Variable("y")).derivative(new Variable("x")));
+  }
+
+  @Test
+  void testDiv() {
+    assertEquals(
+        new Div(new Sub(new Mul(new Number(1), new Variable("y")), new Mul(new Variable("x"), new Number(0))),
+            new Mul(new Variable("y"), new Variable("y"))),
+        new Div(new Variable("x"), new Variable("y")).derivative(new Variable("x")));
+  }
+
+  @Test
+  void testExpr() {
+    Expression expected = new Add(
+        new Number(1),
+        new Add(
+            new Number(1),
+            new Add(
+                new Number(0),
+                new Add(
+                    new Number(0),
+                    new Add(
+                        new Number(0),
+                        new Sub(
+                            new Add(
+                                new Mul(new Number(0), new Variable("x")),
+                                new Mul(new Number(2), new Number(1))),
+                            new Add(
+                                new Number(1),
+                                new Div(
+                                    new Sub(
+                                        new Mul(new Number(1), new Variable("x")),
+                                        new Mul(new Variable("x"), new Number(1))),
+                                    new Mul(new Variable("x"), new Variable("x"))))))))));
+
+    Expression original = new Add(
+        new Variable("x"),
+        new Add(
+            new Variable("x"),
+            new Add(
+                new Variable("y"),
+                new Add(
+                    new Variable("z"),
+                    new Add(
+                        new Number(1),
+                        new Sub(
+                            new Mul(new Number(2), new Variable("x")),
+                            new Add(
+                                new Variable("x"),
+                                new Div(new Variable("x"), new Variable("x")))))))));
+
+    assertEquals(expected, original.derivative(new Variable("x")));
+  }
 }
--- Task_1_1_3/lib/src/test/java/sys/pro/PrintTest.java
+++ Task_1_1_3/lib/src/test/java/sys/pro/PrintTest.java
@@ -7,6 +7,34 @@ import org.junit.jupiter.api.Test;
 /** PrintTest. */
 public class PrintTest {
   @Test
+  void testAdd() {
+    String expected = "(x + y)";
+    Expression expr = new Add(new Variable("x"), new Variable("y"));
+    assertEquals(expected, expr.print());
+  }
+  
+  @Test
+  void testSub() {
+    String expected = "(x - y)";
+    Expression expr = new Sub(new Variable("x"), new Variable("y"));
+    assertEquals(expected, expr.print());
+  }
+  
+  @Test
+  void testMul() {
+    String expected = "(x * y)";
+    Expression expr = new Mul(new Variable("x"), new Variable("y"));
+    assertEquals(expected, expr.print());
+  }
+
+  @Test
+  void testDiv() {
+    String expected = "(x / y)";
+    Expression expr = new Div(new Variable("x"), new Variable("y"));
+    assertEquals(expected, expr.print());
+  }
+  
+  @Test
   void testExpr() {
     String expected = "((((x + x) / x) + y) * 8)";
     Expression expr = new Mul(
