327799463b1ead8741a41ad2af884f9feec9de83
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-10-13 22:51:31 +0700

    fix

--- Task_1_2_1/lib/src/main/java/sys/pro/Graph.java
+++ Task_1_2_1/lib/src/main/java/sys/pro/Graph.java
@@ -4,22 +4,62 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public interface Graph {
+    /*
+     * Gets vertex count.
+     * @param index vertex index
+     * @returns count
+     */
     public long vertexesCount();
 
+    /*
+     * Creates new vertex and returns its vertex.
+     * @param index vertex index
+     * @returns index
+     */
     public int newVertex();
 
+    /*
+     * Removes vertex.
+     * @param index vertex index
+     */
     public void removeVertex(int index);
 
+    /*
+     * Adds directed edge.
+     * @param from 'from' vertex
+     * @param to 'to' vertex
+     */
     public void addDirectedEdge(int from, int to);
 
+    /*
+     * Removes directed edge.
+     * @param from 'from' vertex
+     * @param to 'to' vertex
+     */
     public void removeDirectedEdge(int from, int to);
 
+    /*
+     * Gets all vertexes.
+     * @returns result stream
+     */
     public Stream<Integer> getAllVertexes();
 
+    /*
+     * Gets adjacent vertexes.
+     * @returns result stream
+     */
     public Stream<Integer> getAdjacentVertexes(int index);
 
+    /*
+     * Topological sort.
+     * @returns result stream
+     */
     public Stream<Integer> topSort();
 
+    /*
+     * Reads graph from scanner.
+     * @param Scanner scanner
+     */
     public default void readFromScanner(Scanner scanner) {
         while (scanner.hasNext()) {
             String[] line = scanner.nextLine().strip().split(" ");
