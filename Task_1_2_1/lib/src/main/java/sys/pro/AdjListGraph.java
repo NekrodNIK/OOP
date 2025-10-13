0d87d27d37421b40dbe6f9046374a4e80d9ce655
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-10-13 23:09:16 +0700

    fix

--- Task_1_2_1/lib/src/main/java/sys/pro/AdjListGraph.java
+++ Task_1_2_1/lib/src/main/java/sys/pro/AdjListGraph.java
@@ -7,132 +7,133 @@ import java.util.stream.Stream;
 
 /** AdjListGraph. */
 public class AdjListGraph implements Graph {
-  protected ArrayList<ArrayList<Integer>> adj;
-  protected int vertexesCount;
+    protected ArrayList<ArrayList<Integer>> adj;
+    protected int vertexesCount;
 
-  public AdjListGraph() {
-    adj = new ArrayList<ArrayList<Integer>>();
-  }
-
-  @Override
-  public int newVertex() {
-    adj.addLast(new ArrayList<Integer>());
-    vertexesCount += 1;
-    return adj.size() - 1;
-  }
-
-  @Override
-  public void removeVertex(int index) {
-    for (int i = 0; i < adj.size(); i++) {
-      if (!adj.get(i).isEmpty()) {
-        adj.get(i).remove(Integer.valueOf(index));
-      }
+    public AdjListGraph() {
+        adj = new ArrayList<ArrayList<Integer>>();
     }
 
-    adj.get(index).clear();
-    adj.get(index).add(-1);
-    vertexesCount -= 1;
-  }
-
-  @Override
-  public void addDirectedEdge(int from, int to) {
-    while (adj.size() <= Math.max(from, to)) {
-      newVertex();
+    @Override
+    public int newVertex() {
+        adj.addLast(new ArrayList<Integer>());
+        vertexesCount += 1;
+        return adj.size() - 1;
     }
 
-    if (adj.get(from).size() > 0 && adj.get(from).get(0) == -1) {
-      adj.get(from).clear();
-      vertexesCount += 1;
+    @Override
+    public void removeVertex(int index) {
+        for (int i = 0; i < adj.size(); i++) {
+            if (!adj.get(i).isEmpty()) {
+                adj.get(i).remove(Integer.valueOf(index));
+            }
+        }
+
+        adj.get(index).clear();
+        adj.get(index).add(-1);
+        vertexesCount -= 1;
     }
 
-    if (adj.get(to).size() > 0 && adj.get(to).get(0) == -1) {
-      adj.get(to).clear();
-      vertexesCount += 1;
+    @Override
+    public void addDirectedEdge(int from, int to) {
+        while (adj.size() <= Math.max(from, to)) {
+            newVertex();
+        }
+
+        if (adj.get(from).size() > 0 && adj.get(from).get(0) == -1) {
+            adj.get(from).clear();
+            vertexesCount += 1;
+        }
+
+        if (adj.get(to).size() > 0 && adj.get(to).get(0) == -1) {
+            adj.get(to).clear();
+            vertexesCount += 1;
+        }
+
+        adj.get(from).add(to);
     }
 
-    adj.get(from).add(to);
-  }
-
-  @Override
-  public void removeDirectedEdge(int from, int to) {
-    adj.get(from).remove(Integer.valueOf(to));
-  }
-
-  @Override
-  public Stream<Integer> getAllVertexes() {
-    return IntStream.range(0, adj.size())
-        .mapToObj((item) -> Integer.valueOf(item))
-        .filter((index) -> adj.get(index).size() == 0 || adj.get(index).get(0) != -1);
-  }
-
-  @Override
-  public Stream<Integer> getAdjacentVertexes(int index) {
-    if (adj.get(index).size() > 0 && adj.get(index).get(0) == -1) {
-      return Stream.empty();
-    }
-    
-    return adj.get(index).stream();
-  }
-
-  @Override
-  public long vertexesCount() {
-    return vertexesCount;
-  }
-
-  private void dfs(List<Integer> result, List<Boolean> visited, Integer v) {
-    visited.set(v, true);
-    for (int u : adj.get(v)) {
-      if (!visited.get(u)) {
-        dfs(result, visited, u);
-      }
+    @Override
+    public void removeDirectedEdge(int from, int to) {
+        adj.get(from).remove(Integer.valueOf(to));
     }
 
-    result.add(v);
-  }
-
-  @Override
-  public Stream<Integer> topSort() {
-    ArrayList<Integer> result = new ArrayList<Integer>();
-
-    ArrayList<Boolean> visited = new ArrayList<Boolean>();
-    for (int i = 0; i < adj.size(); i++) {
-      visited.add(false);
+    @Override
+    public Stream<Integer> getAllVertexes() {
+        return IntStream.range(0, adj.size())
+                .mapToObj((item) -> Integer.valueOf(item))
+                .filter((index) -> adj.get(index).size() == 0 || adj.get(index).get(0) != -1);
     }
 
-    getAllVertexes().filter((v) -> !visited.get(v)).forEach((v) -> dfs(result, visited, v));
-    return result.reversed().stream();
-  }
+    @Override
+    public Stream<Integer> getAdjacentVertexes(int index) {
+        if (adj.get(index).size() > 0 && adj.get(index).get(0) == -1) {
+            return Stream.empty();
+        }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (!(obj instanceof Graph)) {
-      return false;
+        return adj.get(index).stream();
     }
 
-    Graph graph = (Graph) obj;
-    List<Integer> vertexes = this.getAllVertexes().toList();
-
-    if (!vertexes.equals(graph.getAllVertexes().toList())) {
-      return false;
+    @Override
+    public long vertexesCount() {
+        return vertexesCount;
     }
 
-    for (Integer v : vertexes) {
-      if (!this.getAdjacentVertexes(v)
-          .toList()
-          .equals(graph.getAdjacentVertexes(v).toList())) {
-        return false;
-      }
+    private void dfs(List<Integer> result, List<Boolean> visited, Integer v) {
+        visited.set(v, true);
+        for (int u : adj.get(v)) {
+            if (!visited.get(u)) {
+                dfs(result, visited, u);
+            }
+        }
+
+        result.add(v);
     }
 
-    return true;
-  }
+    @Override
+    public Stream<Integer> topSort() {
+        ArrayList<Integer> result = new ArrayList<Integer>();
 
-  @Override
-  public String toString() {
-    return getAllVertexes()
-        .flatMap(
-            (from) -> getAdjacentVertexes(from)
-                .map((to) -> "%d %d\n".formatted(from, to)))
-        .reduce("", (x, y) -> x + y);
-  }
+        ArrayList<Boolean> visited = new ArrayList<Boolean>();
+        for (int i = 0; i < adj.size(); i++) {
+            visited.add(false);
+        }
+
+        getAllVertexes().filter((v) -> !visited.get(v)).forEach((v) -> dfs(result, visited, v));
+        return result.reversed().stream();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Graph)) {
+            return false;
+        }
+
+        Graph graph = (Graph) obj;
+        List<Integer> vertexes = this.getAllVertexes().toList();
+
+        if (!vertexes.equals(graph.getAllVertexes().toList())) {
+            return false;
+        }
+
+        for (Integer v : vertexes) {
+            if (!this.getAdjacentVertexes(v)
+                    .toList()
+                    .equals(graph.getAdjacentVertexes(v).toList())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return getAllVertexes()
+                .flatMap(
+                        (from) ->
+                                getAdjacentVertexes(from)
+                                        .map((to) -> "%d %d\n".formatted(from, to)))
+                .reduce("", (x, y) -> x + y);
+    }
 }
--- Task_1_2_1/lib/src/test/java/sys/pro/GraphTest.java
+++ Task_1_2_1/lib/src/test/java/sys/pro/GraphTest.java
@@ -16,292 +16,293 @@ import java.util.stream.Stream;
 
 /** GraphTest. */
 public class GraphTest {
-  private static Stream<Class<? extends Graph>> getImplementations() {
-    return Stream.of(AdjListGraph.class, AdjMatrixGraph.class, IncidenceMatrixGraph.class);
-  }
+    private static Stream<Class<? extends Graph>> getImplementations() {
+        return Stream.of(AdjListGraph.class, AdjMatrixGraph.class, IncidenceMatrixGraph.class);
+    }
 
-  private static Stream<Arguments> getImplementationsPairs() {
-    ArrayList<Class<? extends Graph>> arr = new ArrayList<Class<? extends Graph>>();
-    arr.add(AdjListGraph.class);
-    arr.add(AdjMatrixGraph.class);
-    arr.add(IncidenceMatrixGraph.class);
+    private static Stream<Arguments> getImplementationsPairs() {
+        ArrayList<Class<? extends Graph>> arr = new ArrayList<Class<? extends Graph>>();
+        arr.add(AdjListGraph.class);
+        arr.add(AdjMatrixGraph.class);
+        arr.add(IncidenceMatrixGraph.class);
 
-    ArrayList<Arguments> result = new ArrayList<Arguments>();
+        ArrayList<Arguments> result = new ArrayList<Arguments>();
 
-    for (Class<? extends Graph> cls1 : arr) {
-      for (Class<? extends Graph> cls2 : arr) {
-        for (Class<? extends Graph> cls3 : arr) {
-          result.add(Arguments.of(cls1, cls2, cls3));
+        for (Class<? extends Graph> cls1 : arr) {
+            for (Class<? extends Graph> cls2 : arr) {
+                for (Class<? extends Graph> cls3 : arr) {
+                    result.add(Arguments.of(cls1, cls2, cls3));
+                }
+            }
         }
-      }
-    }
-    return result.stream();
-  }
-
-  @ParameterizedTest
-  @MethodSource("getImplementations")
-  void testAddDirectedEdge(Class<? extends Graph> cls) {
-    Graph graph;
-
-    try {
-      graph = cls.getDeclaredConstructor().newInstance();
-    } catch (InstantiationException
-        | IllegalAccessException
-        | InvocationTargetException
-        | NoSuchMethodException e) {
-      fail();
-      return;
+        return result.stream();
     }
 
-    Integer from = graph.newVertex();
-    Integer to = graph.newVertex();
-    graph.addDirectedEdge(from, to);
+    @ParameterizedTest
+    @MethodSource("getImplementations")
+    void testAddDirectedEdge(Class<? extends Graph> cls) {
+        Graph graph;
 
-    assertEquals(to, graph.getAdjacentVertexes(from).findFirst().get());
-  }
+        try {
+            graph = cls.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
+            fail();
+            return;
+        }
 
-  @ParameterizedTest
-  @MethodSource("getImplementations")
-  void testRemoveDirectedEdge(Class<? extends Graph> cls) {
-    Graph graph;
+        Integer from = graph.newVertex();
+        Integer to = graph.newVertex();
+        graph.addDirectedEdge(from, to);
 
-    try {
-      graph = cls.getDeclaredConstructor().newInstance();
-    } catch (InstantiationException
-        | IllegalAccessException
-        | InvocationTargetException
-        | NoSuchMethodException e) {
-      fail();
-      return;
+        assertEquals(to, graph.getAdjacentVertexes(from).findFirst().get());
     }
 
-    Integer from = graph.newVertex();
-    Integer to = graph.newVertex();
-    graph.addDirectedEdge(from, to);
-    assertEquals(to, graph.getAdjacentVertexes(from).findFirst().get());
+    @ParameterizedTest
+    @MethodSource("getImplementations")
+    void testRemoveDirectedEdge(Class<? extends Graph> cls) {
+        Graph graph;
 
-    graph.removeDirectedEdge(from, to);
-    assertTrue(graph.getAdjacentVertexes(from).findFirst().isEmpty());
-  }
+        try {
+            graph = cls.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
+            fail();
+            return;
+        }
 
-  @ParameterizedTest
-  @MethodSource("getImplementations")
-  void testRemoveVertex(Class<? extends Graph> cls) {
-    Graph graph;
+        Integer from = graph.newVertex();
+        Integer to = graph.newVertex();
+        graph.addDirectedEdge(from, to);
+        assertEquals(to, graph.getAdjacentVertexes(from).findFirst().get());
 
-    try {
-      graph = cls.getDeclaredConstructor().newInstance();
-    } catch (InstantiationException
-        | IllegalAccessException
-        | InvocationTargetException
-        | NoSuchMethodException e) {
-      fail();
-      return;
+        graph.removeDirectedEdge(from, to);
+        assertTrue(graph.getAdjacentVertexes(from).findFirst().isEmpty());
     }
 
-    Integer from = graph.newVertex();
-    Integer to = graph.newVertex();
-    graph.addDirectedEdge(from, to);
-    assertEquals(to, graph.getAdjacentVertexes(from).findFirst().get());
+    @ParameterizedTest
+    @MethodSource("getImplementations")
+    void testRemoveVertex(Class<? extends Graph> cls) {
+        Graph graph;
 
-    graph.removeVertex(from);
-    assertTrue(graph.getAdjacentVertexes(from).findFirst().isEmpty());
-  }
+        try {
+            graph = cls.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
+            fail();
+            return;
+        }
 
-  @ParameterizedTest
-  @MethodSource("getImplementations")
-  void testTopSortSimple(Class<? extends Graph> cls) {
-    Graph graph;
+        Integer from = graph.newVertex();
+        Integer to = graph.newVertex();
+        graph.addDirectedEdge(from, to);
+        assertEquals(to, graph.getAdjacentVertexes(from).findFirst().get());
 
-    try {
-      graph = cls.getDeclaredConstructor().newInstance();
-    } catch (InstantiationException
-        | IllegalAccessException
-        | InvocationTargetException
-        | NoSuchMethodException e) {
-      fail();
-      return;
+        graph.removeVertex(from);
+        assertTrue(graph.getAdjacentVertexes(from).findFirst().isEmpty());
     }
 
-    ArrayList<Integer> expected = new ArrayList<Integer>();
+    @ParameterizedTest
+    @MethodSource("getImplementations")
+    void testTopSortSimple(Class<? extends Graph> cls) {
+        Graph graph;
 
-    expected.add(graph.newVertex());
-    expected.add(graph.newVertex());
-    graph.addDirectedEdge(expected.get(expected.size() - 2), expected.getLast());
+        try {
+            graph = cls.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
+            fail();
+            return;
+        }
 
-    expected.add(graph.newVertex());
-    graph.addDirectedEdge(expected.get(expected.size() - 2), expected.getLast());
+        ArrayList<Integer> expected = new ArrayList<Integer>();
 
-    expected.add(graph.newVertex());
-    graph.addDirectedEdge(expected.get(expected.size() - 2), expected.getLast());
+        expected.add(graph.newVertex());
+        expected.add(graph.newVertex());
+        graph.addDirectedEdge(expected.get(expected.size() - 2), expected.getLast());
 
-    expected.add(graph.newVertex());
-    graph.addDirectedEdge(expected.get(expected.size() - 2), expected.getLast());
+        expected.add(graph.newVertex());
+        graph.addDirectedEdge(expected.get(expected.size() - 2), expected.getLast());
 
-    expected.add(graph.newVertex());
-    graph.addDirectedEdge(expected.get(expected.size() - 2), expected.getLast());
+        expected.add(graph.newVertex());
+        graph.addDirectedEdge(expected.get(expected.size() - 2), expected.getLast());
 
-    assertEquals(expected, graph.topSort().toList());
-  }
+        expected.add(graph.newVertex());
+        graph.addDirectedEdge(expected.get(expected.size() - 2), expected.getLast());
 
-  @ParameterizedTest
-  @MethodSource("getImplementations")
-  void testTopSort(Class<? extends Graph> cls) {
-    Graph graph;
+        expected.add(graph.newVertex());
+        graph.addDirectedEdge(expected.get(expected.size() - 2), expected.getLast());
 
-    try {
-      graph = cls.getDeclaredConstructor().newInstance();
-    } catch (InstantiationException
-        | IllegalAccessException
-        | InvocationTargetException
-        | NoSuchMethodException e) {
-      fail();
-      return;
+        assertEquals(expected, graph.topSort().toList());
     }
 
-    int v0 = graph.newVertex();
-    int v1 = graph.newVertex();
-    int v2 = graph.newVertex();
-    int v3 = graph.newVertex();
-    int v4 = graph.newVertex();
-    int v5 = graph.newVertex();
-    int v6 = graph.newVertex();
-    int v7 = graph.newVertex();
+    @ParameterizedTest
+    @MethodSource("getImplementations")
+    void testTopSort(Class<? extends Graph> cls) {
+        Graph graph;
 
-    graph.addDirectedEdge(v0, v1);
-    graph.addDirectedEdge(v1, v2);
-    graph.addDirectedEdge(v2, v3);
-    graph.addDirectedEdge(v0, v4);
-    graph.addDirectedEdge(v4, v5);
-    graph.addDirectedEdge(v5, v3);
-    graph.addDirectedEdge(v1, v6);
-    graph.addDirectedEdge(v4, v6);
-    graph.addDirectedEdge(v6, v7);
-    graph.addDirectedEdge(v7, v3);
+        try {
+            graph = cls.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
+            fail();
+            return;
+        }
 
-    int v8 = graph.newVertex();
+        int v0 = graph.newVertex();
+        int v1 = graph.newVertex();
+        int v2 = graph.newVertex();
+        int v3 = graph.newVertex();
+        int v4 = graph.newVertex();
+        int v5 = graph.newVertex();
+        int v6 = graph.newVertex();
+        int v7 = graph.newVertex();
 
-    List<Integer> result = graph.topSort().toList();
+        graph.addDirectedEdge(v0, v1);
+        graph.addDirectedEdge(v1, v2);
+        graph.addDirectedEdge(v2, v3);
+        graph.addDirectedEdge(v0, v4);
+        graph.addDirectedEdge(v4, v5);
+        graph.addDirectedEdge(v5, v3);
+        graph.addDirectedEdge(v1, v6);
+        graph.addDirectedEdge(v4, v6);
+        graph.addDirectedEdge(v6, v7);
+        graph.addDirectedEdge(v7, v3);
 
-    assertTrue(result.contains(v8));
-    assertTrue(result.indexOf(v0) < result.indexOf(v1));
-    assertTrue(result.indexOf(v1) < result.indexOf(v2));
-    assertTrue(result.indexOf(v2) < result.indexOf(v3));
-    assertTrue(result.indexOf(v0) < result.indexOf(v4));
-    assertTrue(result.indexOf(v4) < result.indexOf(v5));
-    assertTrue(result.indexOf(v5) < result.indexOf(v3));
-    assertTrue(result.indexOf(v1) < result.indexOf(v6));
-    assertTrue(result.indexOf(v4) < result.indexOf(v6));
-    assertTrue(result.indexOf(v6) < result.indexOf(v7));
-    assertTrue(result.indexOf(v7) < result.indexOf(v3));
-    assertEquals(9, result.size());
-    assertEquals(9, result.stream().distinct().count());
-  }
+        int v8 = graph.newVertex();
 
-  @ParameterizedTest
-  @MethodSource("getImplementations")
-  void testReadFile(Class<? extends Graph> cls) {
-    Graph graph;
+        List<Integer> result = graph.topSort().toList();
 
-    try {
-      graph = cls.getDeclaredConstructor().newInstance();
-    } catch (InstantiationException
-        | IllegalAccessException
-        | InvocationTargetException
-        | NoSuchMethodException e) {
-      fail();
-      return;
+        assertTrue(result.contains(v8));
+        assertTrue(result.indexOf(v0) < result.indexOf(v1));
+        assertTrue(result.indexOf(v1) < result.indexOf(v2));
+        assertTrue(result.indexOf(v2) < result.indexOf(v3));
+        assertTrue(result.indexOf(v0) < result.indexOf(v4));
+        assertTrue(result.indexOf(v4) < result.indexOf(v5));
+        assertTrue(result.indexOf(v5) < result.indexOf(v3));
+        assertTrue(result.indexOf(v1) < result.indexOf(v6));
+        assertTrue(result.indexOf(v4) < result.indexOf(v6));
+        assertTrue(result.indexOf(v6) < result.indexOf(v7));
+        assertTrue(result.indexOf(v7) < result.indexOf(v3));
+        assertEquals(9, result.size());
+        assertEquals(9, result.stream().distinct().count());
     }
 
-    Scanner scanner = new Scanner(
-        """
-            0 1
-            2 3
-            6 10
-            7 8
-            10 11
-            """);
+    @ParameterizedTest
+    @MethodSource("getImplementations")
+    void testReadFile(Class<? extends Graph> cls) {
+        Graph graph;
 
-    graph.readFromScanner(scanner);
+        try {
+            graph = cls.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
+            fail();
+            return;
+        }
 
-    assertEquals(1, graph.getAdjacentVertexes(0).findFirst().get());
-    assertEquals(3, graph.getAdjacentVertexes(2).findFirst().get());
-    assertEquals(10, graph.getAdjacentVertexes(6).findFirst().get());
-    assertEquals(11, graph.getAdjacentVertexes(10).findFirst().get());
-    assertEquals(8, graph.getAdjacentVertexes(7).findFirst().get());
-  }
+        Scanner scanner =
+                new Scanner(
+                        """
+                        0 1
+                        2 3
+                        6 10
+                        7 8
+                        10 11
+                        """);
 
-  @ParameterizedTest
-  @MethodSource("getImplementationsPairs")
-  void testEquals(Class<? extends Graph> cls1, Class<? extends Graph> cls2) {
-    Graph graph1;
-    Graph graph2;
+        graph.readFromScanner(scanner);
 
-    try {
-      graph1 = cls1.getDeclaredConstructor().newInstance();
-      graph2 = cls1.getDeclaredConstructor().newInstance();
-    } catch (InstantiationException
-        | IllegalAccessException
-        | InvocationTargetException
-        | NoSuchMethodException e) {
-      fail();
-      return;
+        assertEquals(1, graph.getAdjacentVertexes(0).findFirst().get());
+        assertEquals(3, graph.getAdjacentVertexes(2).findFirst().get());
+        assertEquals(10, graph.getAdjacentVertexes(6).findFirst().get());
+        assertEquals(11, graph.getAdjacentVertexes(10).findFirst().get());
+        assertEquals(8, graph.getAdjacentVertexes(7).findFirst().get());
     }
 
-    graph1.addDirectedEdge(0, 1);
-    graph2.addDirectedEdge(0, 1);
+    @ParameterizedTest
+    @MethodSource("getImplementationsPairs")
+    void testEquals(Class<? extends Graph> cls1, Class<? extends Graph> cls2) {
+        Graph graph1;
+        Graph graph2;
 
-    graph1.addDirectedEdge(3, 4);
-    graph2.addDirectedEdge(3, 4);
+        try {
+            graph1 = cls1.getDeclaredConstructor().newInstance();
+            graph2 = cls1.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
+            fail();
+            return;
+        }
 
-    graph1.addDirectedEdge(5, 1);
-    graph2.addDirectedEdge(5, 1);
+        graph1.addDirectedEdge(0, 1);
+        graph2.addDirectedEdge(0, 1);
 
-    graph1.addDirectedEdge(10, 2);
-    graph2.addDirectedEdge(10, 2);
+        graph1.addDirectedEdge(3, 4);
+        graph2.addDirectedEdge(3, 4);
 
-    graph1.addDirectedEdge(2, 1);
-    graph2.addDirectedEdge(2, 1);
+        graph1.addDirectedEdge(5, 1);
+        graph2.addDirectedEdge(5, 1);
 
-    graph1.addDirectedEdge(2, 3);
-    graph2.addDirectedEdge(2, 3);
+        graph1.addDirectedEdge(10, 2);
+        graph2.addDirectedEdge(10, 2);
 
-    graph1.addDirectedEdge(5, 10);
-    graph2.addDirectedEdge(5, 10);
+        graph1.addDirectedEdge(2, 1);
+        graph2.addDirectedEdge(2, 1);
 
-    assertEquals(graph1, graph2);
-  }
+        graph1.addDirectedEdge(2, 3);
+        graph2.addDirectedEdge(2, 3);
 
-  @ParameterizedTest
-  @MethodSource("getImplementations")
-  void testToString(Class<? extends Graph> cls) {
-    Graph graph;
+        graph1.addDirectedEdge(5, 10);
+        graph2.addDirectedEdge(5, 10);
 
-    try {
-      graph = cls.getDeclaredConstructor().newInstance();
-    } catch (InstantiationException
-        | IllegalAccessException
-        | InvocationTargetException
-        | NoSuchMethodException e) {
-      fail();
-      return;
+        assertEquals(graph1, graph2);
     }
 
-    graph.addDirectedEdge(0, 1);
-    graph.addDirectedEdge(3, 4);
-    graph.addDirectedEdge(5, 1);
-    graph.addDirectedEdge(10, 2);
-    graph.addDirectedEdge(2, 1);
-    graph.addDirectedEdge(2, 3);
-    graph.addDirectedEdge(5, 10);
+    @ParameterizedTest
+    @MethodSource("getImplementations")
+    void testToString(Class<? extends Graph> cls) {
+        Graph graph;
 
-    assertTrue(
-        graph.toString().contains("0 1")
-            && graph.toString().contains("3 4")
-            && graph.toString().contains("5 1")
-            && graph.toString().contains("10 2")
-            && graph.toString().contains("2 1")
-            && graph.toString().contains("2 3")
-            && graph.toString().contains("5 10"));
-  }
+        try {
+            graph = cls.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
+            fail();
+            return;
+        }
+
+        graph.addDirectedEdge(0, 1);
+        graph.addDirectedEdge(3, 4);
+        graph.addDirectedEdge(5, 1);
+        graph.addDirectedEdge(10, 2);
+        graph.addDirectedEdge(2, 1);
+        graph.addDirectedEdge(2, 3);
+        graph.addDirectedEdge(5, 10);
+
+        assertTrue(
+                graph.toString().contains("0 1")
+                        && graph.toString().contains("3 4")
+                        && graph.toString().contains("5 1")
+                        && graph.toString().contains("10 2")
+                        && graph.toString().contains("2 1")
+                        && graph.toString().contains("2 3")
+                        && graph.toString().contains("5 10"));
+    }
 }
