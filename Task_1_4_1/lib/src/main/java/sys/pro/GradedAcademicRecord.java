dcddc46e0864765c32b554fc12bd3fedea837ad1
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-10-28 11:14:28 +0700

    fix

---
+++ Task_1_4_1/lib/src/main/java/sys/pro/GradedAcademicRecord.java
@@ -0,0 +1,26 @@
+package sys.pro;
+
+/** GradedAcademicRecord. */
+public abstract class GradedAcademicRecord extends AcademicRecord {
+  protected Grade grade;
+
+  /**
+   * Constructor.
+   *
+   * @param subject subject name
+   * @param grade   grade
+   */
+  public GradedAcademicRecord(String subject, Grade grade) {
+    super(subject);
+    this.grade = grade;
+  }
+
+  /**
+   * Get grade.
+   *
+   * @return result
+   */
+  public Grade getGrade() {
+    return grade;
+  }
+}
